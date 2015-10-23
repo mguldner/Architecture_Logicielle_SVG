d4b055bc0ef6e0ed4cc24e172224ae23e6c7f8e6
plegault <pierre.legault64@gmail.com>
2015-10-23 12:52:16 +0200

    Changing Path methods

--- src/dataStructure/Path.java
+++ src/dataStructure/Path.java
@@ -9,6 +9,19 @@ public abstract class Path {
 		return points;
 	}
 	
-	public abstract Point2D[] generatePath();
+	/*============ Functions dedicated to each export mode ============*/
+	/* The idea here is to create functions that return what is "needed" to apply the drawing.
+	 * 
+	 * Basically, for a svg we need a string that will be inserted in a svg component.
+	 * 
+	 * On the other hand, for a Java export that will be executed, we need the points that will be used by
+	 * the library to generate a drawing.
+	 **/
+	
+	/**
+	 * @return a string that represents the "path part" of a svg component
+	 */
+	public abstract String generateSvgPath();
+	public abstract Point2D[] generateJavaPath();
 
 }
--- src/dataStructure/paths/PolygonalPath.java
+++ src/dataStructure/paths/PolygonalPath.java
@@ -6,7 +6,13 @@ import utils.Point2D;
 public class PolygonalPath extends Path {
 
 	@Override
-	public Point2D[] generatePath() {
+	public String generateSvgPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point2D[] generateJavaPath() {
 		return this.getPoints();
 	}
 
