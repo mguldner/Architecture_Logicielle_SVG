f07d217df92568f40d3b7d14f383d7cd981d30d6
mguldner <mathieuguldner.emn@gmail.com>
2015-10-26 16:34:39 +0100

    Implementation of Fill.java

---
+++ src/datastructure/actions/Fill.java
@@ -0,0 +1,65 @@
+package datastructure.actions;
+
+import datastructure.Action;
+import datastructure.Path;
+import utils.UsefulFunctions;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+/**
+ * This class is the representation of one type of Action.
+ * This action draws a path with a tool.
+ */
+public class Fill extends Action {
+  /*==============================*/
+  /*========== Variables =========*/
+  /*==============================*/
+  /**
+   * path: Path to follow.
+   * tool: Tool to use.
+   */
+  private Path path;
+  private int[] rgbColorCode;
+  
+  /*=================================*/
+  /*========== Constructors =========*/
+  /*=================================*/
+  public Fill(Path path, String hexaCode) {
+    this.path = path;
+    this.rgbColorCode = UsefulFunctions.convertHexaToRgb(hexaCode);
+  }
+  
+  
+  /*========================================*/
+  /*============ Shared Methods ============*/
+  /*========================================*/
+
+  
+  /*===============================================================*/
+  /*============ Methods dedicated to each export mode ============*/
+  /*===============================================================*/
+  
+  /*=================================================*/
+  /*================== SVG export ===================*/
+  /*=================================================*/
+  @Override
+  public String applySvgAction() {
+    String svgCode = "<path ";
+    svgCode += path.generateSvgPath() + " ";
+    svgCode += "style=\"rgb(" + rgbColorCode[0] + "," 
+      + rgbColorCode[1] + "," + rgbColorCode[2] + ")\" ";
+    svgCode += "/>\n";
+    return svgCode;
+  }
+
+  /*=================================================*/
+  /*================== Java export ==================*/
+  /*=================================================*/
+  @Override
+  public void applyJavaAction(Graphics2D graph) {
+    graph.setColor(new Color(rgbColorCode[0], rgbColorCode[1], rgbColorCode[2]));
+    //graph.fill(path.generateJavaPath(new Graphics2D()));
+  }
+
+}
