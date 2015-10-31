0a31c3a29aa2ce3d43d68e0491a417d97cc5d9c0
mguldner <mathieuguldner.emn@gmail.com>
2015-10-31 18:32:57 +0100

    Create and test (for JavaRendering)  the insertion

--- src/main/Main.java
+++ src/main/Main.java
@@ -53,10 +53,24 @@ public class Main {
         new Point2D(18, 6),
         new Point2D(150,56)
     };
+    Point2D[] points4 = {
+        new Point2D(10, 10),
+        new Point2D(70, 10),
+        new Point2D(70, 70),
+        new Point2D(10, 70)
+    };
+    Point2D[] points5 = {
+        new Point2D(20, 20),
+        new Point2D(50, 20),
+        new Point2D(50, 50),
+        new Point2D(20, 50)
+    };
     Path polygonalPath1 = new PolygonalPath(points1, false);
     Path polygonalPath2 = new PolygonalPath(points2, false);
     Path polygonalPath3 = new PolygonalPath(points3, true);
-    Path[] paths = {polygonalPath3};
+    Path polygonalPath4 = new PolygonalPath(points4, true);
+    Path polygonalPath5 = new PolygonalPath(points5, true);
+    Path[] paths = {polygonalPath5};
     
     
     /*=========================*/
@@ -65,13 +79,15 @@ public class Main {
     Drawing draw1 = new Draw(polygonalPath1, redPen);
     Drawing draw2 = new Draw(polygonalPath2, greenPen);
     Drawing fill1 = new Fill(polygonalPath3, greenCm);
-    Drawing insert1 = new Insert(draw1, paths);
+    Drawing fill2 = new Fill(polygonalPath4, redCm);
+    Drawing insert1 = new Insert(fill1, paths);
     Drawing label1 = new Label("Hello world", new Point2D(100, 100), (TextTool)textTool);
     Drawing[] drawingArray = {
         //draw1,
         //draw2,
         //fill1,
-        //insert1,
+        fill2,
+        insert1,
         label1
     };
     
